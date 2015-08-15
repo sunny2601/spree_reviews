0a66bfcfb79554cf9065ff237cb8d9c392b582dd
sunny2601 <sunnysarrot@gmail.com>
2015-08-16 00:54:59 +0530

    Update add_reviews_after_product_properties.rb

--- app/overrides/add_reviews_after_product_properties.rb
+++ app/overrides/add_reviews_after_product_properties.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/products/show",
                      :name => "converted_product_properties_767643482",
-                     :insert_after => "[data-hook='thumbnails-prop']",
+                     :insert_after => "[data-hook='thumbnails']",
                      :partial => "spree/shared/reviews",
                      :disabled => false)
